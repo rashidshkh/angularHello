7ec63d97262034407ff1f4eb8961582807764ae7
rashidshkh <rashidshkh11@gmail.com>
2017-12-11 21:58:48 +0530

    started with learning

---
+++ src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { Article } from './article/article.model'; // <-- import this
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  articles: Article[];
+  //title = 'app works!';
+  addArticle(title: HTMLInputElement, link: HTMLInputElement): boolean {
+    console.log(`Adding article title: ${title.value} and link: ${link.value}`);
+    return false;
+  }
+}
---
+++ src/app/article/article.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit, HostBinding } from '@angular/core';
+import { Article } from './article.model';
+
+@Component({
+  selector: 'app-article',
+  templateUrl: './article.component.html',
+  styleUrls: ['./article.component.css']
+})
+export class ArticleComponent implements OnInit {
+  @HostBinding('attr.class') cssClass = 'row';
+  article : Article;
+
+
+  constructor() {
+    this.article = new Article(
+      'Angular 2',
+      'http://angular.io',
+      10);
+    
+  }
+
+  voteUp() {
+    this.article.voteUp();
+    return false;
+  }
+  voteDown() {
+    this.article.voteDown();
+    return false;
+  }
+
+  ngOnInit() {
+  }
+
+}
